695262428d99eb98143f5b1057cf2d491d7c0ea0
hai <nguyentienhai@hotmail.com.vn>
2025-08-17 22:23:24 +0700

    add rev2

--- keymaps/vial/config.h
+++ keymaps/vial/config.h
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 // #define USE_I2C
 /* Select hand configuration */
-#define MASTER_LEFT
-//#define MASTER_RIGHT
+//#define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 #define VIAL_KEYBOARD_UID {0x54, 0xB9, 0x18, 0x3D, 0xE9, 0x29, 0xA8, 0x22}
 #define DYNAMIC_KEYMAP_LAYER_COUNT 10
--- rev1/config.h
+++ rev1/config.h
@@ -24,8 +24,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 7
 
 // wiring of each half
-#define MATRIX_ROW_PINS { GP2, GP4, GP6, GP17, GP18 }
-#define MATRIX_COL_PINS { GP7, GP10, GP13, GP16, GP5, GP3,GP1 }
+#define MATRIX_ROW_PINS { GP5, GP6, GP14, GP26, GP28 }
+#define MATRIX_ROW_PINS_RIGHT {GP14, GP12, GP6, GP4, GP2}
+#define MATRIX_COL_PINS { GP1, GP2, GP3, GP4, GP12, GP27,GP29 }
+#define MATRIX_COL_PINS_RIGHT {GP29, GP28, GP27, GP26, GP5, GP3, GP1}
 // #define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6, F5} //uncomment this line and comment line above if you need to reverse left-to-right key order
 //#define MATRIX_COL_PINS {GP28, GP27, GP26, GP24, GP23, GP22, GP16, GP13, GP7, GP6, GP5, GP4, GP3, GP2}
 /* COL2ROW or ROW2COL */
@@ -39,21 +41,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
-#define WS2812_DI_PIN GP20
-#define RGBLED_NUM 24
-#define RGBLED_SPLIT {12, 12}
-#define RGBLIGHT_LED_COUNT RGB_NUM
-
+#define WS2812_DI_PIN GP15
+#define RGBLED_NUM 80
+#define RGBLED_SPLIT {40, 40}
+#define RGBLIGHT_LED_COUNT RGBLED_NUM
 /* serial.c configuration for split keyboard */
 //#define WS2812_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the WS2812 implementation uses the PIO0 peripheral
-#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
-#define SERIAL_USART_TX_PIN GP28     // USART TX pin
-#define SERIAL_USART_RX_PIN GP29     // USART RX pin
+/* #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode. */
+#define SERIAL_USART_TX_PIN GP8     // USART TX pin
+/* #define SERIAL_USART_RX_PIN GP9     // USART RX pin */
 #define SERIAL_PIO_USE_PIO1
+#define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 64
 
 //
 #define I2C_DRIVER I2CD1
-#define I2C1_SDA_PIN GP26
-#define I2C1_SCL_PIN GP27
-#define ENCODERS_PAD_A { GP11 }
-#define ENCODERS_PAD_B { GP12 }
+#define I2C1_SDA_PIN GP11
+#define I2C1_SCL_PIN GP10
+#define ENCODERS_PAD_A { GP7 }
+#define ENCODERS_PAD_B { GP13 }
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
